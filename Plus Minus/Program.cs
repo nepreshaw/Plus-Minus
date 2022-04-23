49dc1118c4970882b6ade2ee375cf0bf0d2d5c88
nickp_000 <nickp_000@NICK>
2022-04-23 12:24:36 -0400

    Add project files.

---
+++ Plus Minus/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Plus_Minus
+{
+    class Program
+    {
+        public static void plusMinus(List<int> arr)
+        {
+            decimal length = arr.Count;
+            decimal pos = 0;
+            decimal neg = 0;
+            decimal zero = 0;
+
+            for (int i = 0; i < length; i++)
+            {
+                if (arr[i] > 0)
+                {
+                    pos++;
+                }
+                if (arr[i] == 0)
+                {
+                    zero++;
+                }
+                if (arr[i] < 0)
+                {
+                    neg++;
+                }
+            }
+
+            decimal posRatio = pos / length;
+            decimal negRatio = neg / length;
+            decimal zeroRatio = zero / length;
+
+            Console.WriteLine(posRatio);
+            Console.WriteLine(negRatio);
+            Console.WriteLine(zeroRatio);
+        }
+
+    }
+
+
+}
+}
